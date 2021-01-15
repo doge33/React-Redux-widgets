ccffaccc7818222172203ede21f1d2c4755857d2
Sandy Long <sandylx33@gmail.com>
2021-01-15 01:03:28 -0500

    made axios request in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { queryAllByAltText } from '@testing-library/react';
 
 export default function Search() {
 
   const [term, setTerm] = useState('');
 
+  //you are NOT allowed to mark the function inside useEffect as async await! => work around by 
+  //1. create helper function with async await, and call it. OR
+  //2. immediately invoked function(which can be (async...await...) wrapped in parenthesis). OR
+  //3. use Promises
+
+  //useEffect can't return anything except clean-up functions
   useEffect(() => {
-    console.log('in useEffect once');
-  }, []);
+    const search = async() => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      })
+    };
+    search();
+  }, [term]);
 
   return (
     <div>
